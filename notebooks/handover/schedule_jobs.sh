24c3bfe29968ae858a2c7228fc493dcec6739699
Siyuan Tian <35749864+SiyuanT@users.noreply.github.com>
2019-03-08 16:53:04 +1100

    updated job scripts to run in raijin

---
+++ notebooks/handover/schedule_jobs.sh
@@ -0,0 +1,22 @@
+
+#tiles=(0 199 375 424 537 576 815 817 818 838 839 841 863 864 865 866 889)
+#tiles=(0 199 375 841 863 864 865 866 889)
+
+#tiles=(199 375 424 537 576 815 816 817 818 838 839 841 863 864 865 866 889)
+
+
+
+for ti in $(seq 640  928) #
+#for ti in "${tiles[@]}"
+do
+echo $ti 
+year=2016
+method='NBRdist'
+jobidx=` qsub -v ti=$ti,year=$year,method=$method jobs.pbs`
+jobidx2=` qsub -W depend=afterok:${jobidx} -v ti=$ti,year=$year,method=$method merge_tiles.pbs`
+echo $jobidx $jobidx2
+sleep 1
+
+done
+wait
+
