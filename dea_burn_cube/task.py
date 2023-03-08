4e83abe786f5b3e1064460baa905227a8b8607cd
Sai Ma <Sai.Ma@ga.gov.au>
2023-03-08 01:13:00 +0000

    Add method comment to task module

--- dea_burn_cube/task.py
+++ dea_burn_cube/task.py
@@ -1,6 +1,8 @@
+import calendar
 import datetime
 import logging
 import time
+from typing import Dict
 
 import pandas as pd
 import s3fs
@@ -10,7 +12,27 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
 
 def log_execution_time(func):
+    """
+    Decorator function that logs the execution time of the decorated function.
+
+    Args:
+        func (function): The function to be decorated.
+
+    Returns:
+        function: The decorated function that logs the execution time.
+    """
+
     def wrapper(*args, **kwargs):
+        """
+        Wrapper function that logs the execution time of the decorated function.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            object: Result of the decorated function.
+        """
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
@@ -21,7 +43,20 @@ def log_execution_time(func):
     return wrapper
 
 
-def task_to_ranges(task_id, task_table):
+def task_to_ranges(task_id: str, task_table: str) -> Dict[str, str]:
+    """
+    Retrieves the start and end dates for the given task ID from the specified task table.
+
+    Args:
+        task_id : str
+            The unique identifier of the task.
+        task_table : str
+            The name of the CSV file containing the task information.
+
+    Returns:
+        A dictionary containing the start and end dates for the processing period and mapping period,
+        formatted as strings in the format "YYYY-MM-DD".
+    """
 
     _ = s3fs.S3FileSystem(anon=True)
 
@@ -51,14 +86,26 @@ def task_to_ranges(task_id, task_table):
     return result_dict
 
 
-def dynamic_task_to_ranges(dtime):
-    import calendar
+def dynamic_task_to_ranges(dtime: datetime.datetime) -> Dict[str, str]:
+    """
+    Generates a dictionary containing the start and end dates of the processing and mapping periods for
+    a given datetime.
+
+    Args:
+        dtime: datetime.datetime
+            The datetime to generate processing and mapping periods for.
+
+    Returns:
+        dict: A dictionary containing the start and end dates of the processing and mapping periods.
+            The keys are: "Period Start", "Period End", "Mapping Period Start", and "Mapping Period End".
+    """
 
     period_last_day = calendar.monthrange(dtime.year - 1, dtime.month)[1]
 
     result_dict = {}
 
-    # TODO: change me if we plan to move to 3/2 years processing period
+    # Set the start date of the processing period to 5 years before the given datetime month + 1.
+    # Set the end date of the processing period to the last day of the given datetime month of the previous year.
     result_dict["Period Start"] = datetime.datetime(
         dtime.year - 5, dtime.month + 1, 1
     ).strftime("%Y-%m-%d")
@@ -67,6 +114,9 @@ def dynamic_task_to_ranges(dtime):
     ).strftime("%Y-%m-%d")
 
     mapping_period_last_day = calendar.monthrange(dtime.year, dtime.month)[1]
+
+    # Set the start date of the mapping period to the month + 1 of the previous year of the given datetime.
+    # Set the end date of the mapping period to the last day of the given datetime month.
     result_dict["Mapping Period Start"] = datetime.datetime(
         dtime.year - 1, dtime.month + 1, 1
     ).strftime("%Y-%m-%d")
@@ -77,12 +127,32 @@ def dynamic_task_to_ranges(dtime):
     return result_dict
 
 
-def generate_task(task_id, task_table):
+def generate_task(task_id: str, task_table: str) -> Dict[str, str]:
+    """
+    Generate a dictionary of time ranges based on a given task ID and task table.
+
+    Parameters
+    ----------
+    task_id : str
+        The unique identifier of the task.
+    task_table : str
+        The name of the task table to read the time ranges from.
+
+    Returns
+    -------
+    Dict[str, str]
+        A dictionary containing the start and end times for the processing and mapping periods
+        as strings formatted as "%Y-%m-%d".
+    """
+    # Parse the task ID to get the date
     updated_task_id = f"{task_id.split('-')[0]}-20{task_id.split('-')[1]}"
     dtime = datetime.datetime.strptime(updated_task_id, "%b-%Y")
 
+    # Determine if the task falls within a dynamic or static time range and generate the
+    # corresponding dictionary of time ranges
     if dtime.year > 2023:
         result_dict = dynamic_task_to_ranges(dtime)
     else:
         result_dict = task_to_ranges(task_id, task_table)
+
     return result_dict
