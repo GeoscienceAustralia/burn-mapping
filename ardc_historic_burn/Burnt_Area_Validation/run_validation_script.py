68cc16d3c54e22031b57c59f2998423e283145b3
Sai Ma <supermarkioner@gmail.com>
2024-08-02 02:14:32 +0000

    Try to fix the validation script

--- ardc_historic_burn/Burnt_Area_Validation/run_validation_script.py
+++ ardc_historic_burn/Burnt_Area_Validation/run_validation_script.py
@@ -202,9 +202,19 @@ def generate_result_by_study_site_folder(
     fig, axes = plt.subplots(1, 1, figsize=(12, 12))
     plt.subplots_adjust(left=0.1, right=0.75, top=0.9, bottom=0.1)
 
-    CombineArray[0].plot(
-        ax=axes, levels=[0.5, 1.5, 2.5, 3.5, 4.5], colors=colpac2, add_colorbar=False
+    #CombineArray[0].plot(
+    #    ax=axes, levels=[0.5, 1.5, 2.5, 3.5, 4.5], colors=colpac2, add_colorbar=False
+    #)
+
+    # Example of using contourf instead of plot
+    contour = axes.contourf(
+        CombineArray[0], levels=[0.5, 1.5, 2.5, 3.5, 4.5], colors=colpac2
     )
+
+    # Add colorbar
+    cbar = fig.colorbar(contour, ax=axes)
+
+    plt.show()
 
     strDic = {0: TPStr, 1: FNStr, 2: FPStr, 3: TNStr, 4: PrecStr, 5: RecStr}
     # colDic = {0: colpac2[0], 1: colpac2[3], 2: colpac2[2], 3: colpac2[1]}
