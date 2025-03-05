86796dc013b14e56cd73be0a429e7787e2e2345c
Sai Ma <supermarkioner@gmail.com>
2025-03-05 03:58:19 +0000

    Fix an error

--- dea_burn_cube/task.py
+++ dea_burn_cube/task.py
@@ -113,7 +113,7 @@ def task_to_ranges(task_id: str, task_table: str) -> Dict[str, str]:
 
     with fs.open(f"s3://dea-public-data-dev/projects/burn_cube/configs/{task_table}", mode='r') as f:
         # Read the CSV file into a pandas DataFrame
-        task_map = pd.read_csv(f)
+        task_map = pd.read_csv(f, parse_dates=periods_columns, dayfirst=True)
 
     result_dict = {}
 
